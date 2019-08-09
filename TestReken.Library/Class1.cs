c3ce9923eb204c4d33ba9204855fd73d1d9adbcf
Student Education <Student@rdeducation.be>
2019-08-09 14:36:07 +0200

    test

---
+++ TestReken.Library/Class1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace TestReken.Library
+{
+    public class Clas
+    {
+
+        public static double Berekenen(double getal1, double getal2, string operat)
+        {
+            double result = 0;
+            switch (operat)
+            {
+                case "+":
+
+
+                    result = getal1 + getal2;
+                    break;
+                case "-":
+                    result = getal1 - getal2;
+                    break;
+                case "*":
+                    result = getal1 * getal2;
+                    break;
+                case "/":
+                    result = getal1 / getal2;
+                    break;
+            }
+            return result;
+        }
+        public async static void IsGelijkAan(double eersteGetal, TextBox textBox1, TextBox textBox2, ListBox listbox1, string teken)
+        {
+            if (eersteGetal == 0)
+            {
+                textBox1.Text = textBox1.Text;
+                listbox1.Items.Add(textBox1.Text);
+
+                Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+                Windows.Storage.StorageFile sampleFile = await storageFolder.GetFileAsync("sample.txt");
+
+                await Windows.Storage.FileIO.AppendTextAsync(sampleFile, "\n" + textBox2.Text);
+            }
+            else if (textBox1.Text != "")
+            {
+                textBox1.Text = Convert.ToString(Clas.Berekenen(eersteGetal, double.Parse(textBox1.Text), teken));
+                eersteGetal = 0;
+                teken = "";
+                textBox2.Text = textBox1.Text;
+
+                listbox1.Items.Add(textBox1.Text);
+
+                Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+                Windows.Storage.StorageFile sampleFile = await storageFolder.GetFileAsync("sample.txt");
+
+                await Windows.Storage.FileIO.AppendTextAsync(sampleFile, "\n" + textBox2.Text);
+
+            }
+        }
+        public async static void LoadHistory(ListBox listbox1)
+        {
+            Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            Windows.Storage.StorageFile sampleFile = await storageFolder.GetFileAsync("sample.txt");
+
+            string text = await Windows.Storage.FileIO.ReadTextAsync(sampleFile);
+
+            listbox1.Items.Add(text);
+        }
+
+        public static void Delen(TextBox textBox1, TextBox textBox2, ref string teken, ref double eersteGetal)
+        {
+            if (textBox1.Text != "")
+            {
+                if (teken == "")
+                {
+                    eersteGetal = double.Parse(textBox1.Text);
+                    textBox1.Text = "";
+                    teken = "/";
+                    textBox2.Text += "/";
+                }
+                else
+                {
+                    eersteGetal = Berekenen(eersteGetal, double.Parse(textBox1.Text), teken);
+                    textBox1.Text = "";
+                    teken = "/";
+                    textBox2.Text = eersteGetal + "/";
+                }
+            }
+        }
+        public static void Vermenigvuldigen(TextBox textBox1, TextBox textBox2, ref string teken, ref double eersteGetal)
+        {
+            if (textBox1.Text != "")
+            {
+                if (teken == "")
+                {
+                    eersteGetal = double.Parse(textBox1.Text);
+                    textBox1.Text = "";
+                    teken = "*";
+                    textBox2.Text += "*";
+                }
+                else
+                {
+                    eersteGetal = Clas.Berekenen(eersteGetal, double.Parse(textBox1.Text), teken);
+                    textBox1.Text = "";
+                    teken = "*";
+                    textBox2.Text = eersteGetal + "*";
+                }
+            }
+        }
+        public static void Aftrekken(TextBox textBox1, TextBox textBox2, ref string teken, ref double eersteGetal)
+        {
+            if (textBox1.Text != "")
+            {
+                if (teken == "")
+                {
+                    eersteGetal = double.Parse(textBox1.Text);
+                    textBox1.Text = "";
+                    teken = "-";
+                    textBox2.Text += "-";
+                }
+                else
+                {
+                    eersteGetal = Clas.Berekenen(eersteGetal, double.Parse(textBox1.Text), teken);
+                    textBox1.Text = "";
+                    teken = "-";
+                    textBox2.Text = textBox2.Text + "-";
+                }
+            }
+        }
+        public static void Optellen(TextBox textBox1, TextBox textBox2, ref string teken, ref double eersteGetal)
+        {
+            if (textBox1.Text != "")
+            {
+                if (teken == "")
+                {
+                    eersteGetal = double.Parse(textBox1.Text);
+                    textBox1.Text = "";
+                    teken = "+";
+                    textBox2.Text += "+";
+                }
+                else
+                {
+                    eersteGetal = Clas.Berekenen(eersteGetal, double.Parse(textBox1.Text), teken);
+                    textBox1.Text = "";
+                    teken = "+";
+                    textBox2.Text = eersteGetal + "+";
+                }
+            }
+        }
+        public static void TweedeMacht(TextBox textBox1, TextBox textBox2)
+        {
+            if (textBox1.Text != "")
+            {
+                textBox1.Text = Convert.ToString(double.Parse(textBox1.Text) * double.Parse(textBox1.Text));
+                textBox2.Text += "²";
+            }
+        }
+        public static void Clear(TextBox textBox1, TextBox textBox2, ref string teken, ref double eersteGetal)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            eersteGetal = 0;
+            teken = "";
+        }
+    }
+}
---
+++ test reken/MainPage.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using TestReken.Library;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace test_reken
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+   
+    public sealed partial class MainPage : Page
+    {
+       
+        public MainPage()
+        {
+            
+            this.InitializeComponent();
+        }
+
+        double eersteGetal = 0;
+        string teken = "";
+        public void GetalInput(Button button)
+        {
+            textBox1.Text += button.Content;
+            textBox2.Text += button.Content;
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            GetalInput(button1);
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            GetalInput(button2);
+
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            GetalInput(button3);
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            GetalInput(button4);
+
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+            GetalInput(button5);
+
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+            GetalInput(button6);
+        }
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+            GetalInput(button7);
+        }
+
+        private void Button8_Click(object sender, RoutedEventArgs e)
+        {
+            GetalInput(button8);
+        }
+
+        private void Button9_Click(object sender, RoutedEventArgs e)
+        {
+            GetalInput(button9);
+        }
+
+        private void Button10_Click(object sender, RoutedEventArgs e)
+        {
+            GetalInput(button10);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) //VERMENIGVULDIGEN
+        {
+            Clas.Vermenigvuldigen(textBox1, textBox2, ref teken, ref eersteGetal);
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e) //ISGELIJKAAN
+        {
+            Clas.IsGelijkAan(eersteGetal, textBox1, textBox2, listbox1, teken);
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e) //GESCHIEDENIS OPHALEN
+        {
+            Clas.LoadHistory(listbox1);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) // DELEN
+        {
+            Clas.Delen(textBox1, textBox2, ref teken, ref eersteGetal);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Clas.Aftrekken(textBox1, textBox2, ref teken, ref eersteGetal);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Clas.Optellen(textBox1, textBox2, ref teken, ref eersteGetal);
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Clas.TweedeMacht(textBox1, textBox2);
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            Clas.Clear(textBox1, textBox2, ref teken, ref eersteGetal);
+        }
+    }
+}
